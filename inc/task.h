2f0d78e16c036a06a30c427d386e000978a32645
noBthd <Egorkir0401@gmail.com>
2025-03-19 11:20:44 +0300

    Task class added

---
+++ inc/task.h
@@ -0,0 +1,21 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <string>
+
+class Task {
+    public: 
+        Task();
+        ~Task();
+
+        std::string m_task_name;
+        std::string m_task_text;
+        // time
+        // done/not
+
+
+        // addTask func
+    private:
+};
+
+#endif
